1f60559e5152ec3724ee34a02ebd91ff6f916f3b
rocardho <rocardpp@gmail.com>
2017-05-31 18:23:48 +0200

    first commit with spring boot hello service working :)

---
+++ hola-springboot/src/main/java/com/redhat/examples/holaspringboot/HolaRestController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.redhat.examples.holaspringboot;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author ROCARD
+ */
+@RestController
+@RequestMapping("/api")
+@ConfigurationProperties(prefix = "helloapp")
+public class HolaRestController {
+    private String saying;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/hola", produces = "text/plain")
+    public String hola() throws UnknownHostException {
+        String hostname = null;
+        try {
+            hostname = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            hostname = "unknown";
+        }
+        return saying + " " + hostname;
+    }
+
+    public String getSaying() {
+        return saying;
+    }
+
+    public void setSaying(String saying) {
+        this.saying = saying;
+    }
+}
